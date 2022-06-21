5d92e93cca147737d16df991de4de9c0f06aac85
Luis Fernando <luis.silva9902@gmail.com>
2022-06-21 10:43:27 -0300

    Connection made with the GraphCMS API

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 import App from './App'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
