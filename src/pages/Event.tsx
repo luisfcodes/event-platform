dd0234b75f9cf13c2553324a40001605ec983cfa
Luis Fernando <luis.silva9902@gmail.com>
2022-06-26 21:04:25 -0300

    Add dinamic title in event pages

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -3,19 +3,22 @@ import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
-export function Event(){
+export function Event() {
 
   const { slug } = useParams<{ slug: string }>()
-
+  {slug && (document.title = slug)}
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex flex-1">
-        {slug ? <Video lessonSlug={slug}/> : <div className="flex-1" />}
-        <Sidebar />
-      </main>
-      
-    </div>
-    
+
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex flex-1">
+          {slug ? <Video lessonSlug={slug} /> : <div className="flex-1" />}
+          <Sidebar />
+        </main>
+
+      </div>
+    </>
+
   )
 }
